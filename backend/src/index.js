873e69abcb70532a18ff669607fd1bb0e8824c19
AryanSingh645 <aryansingh645.developer@gmail.com>
2025-04-25 12:35:36 +0530

    feat: prisma added with database configs

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port: ${process.env.PORT}`);
+})
