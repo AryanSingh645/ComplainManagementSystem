0076a7806b1f21082fe805be11d6c3cb6494f8a9
AryanSingh645 <aryansingh645.developer@gmail.com>
2025-04-29 11:40:45 +0530

    Basic Deployment on render

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -5,7 +5,7 @@ import verifyUser from "../middleware/auth.middleware.js";
 const router = Router();
 router.route(`/signup/${process.env.JWT_SECRET}`).post(registerAdmin);
 router.route(`/login`).post(loginAdmin);
-router.route(`/verify`).get(verifyAdmin);
+router.route(`/verify`).get(verifyUser,verifyAdmin);
 router.route('/logout').get(verifyUser, logoutAdmin);
 router.route('/getDashboardData').get(verifyUser, getAdminDashBoard);
 
--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -1,23 +1,30 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
-const ProtectedRoute = ({children}) => {
-    const navigate = useNavigate();
-    useEffect( () => {
-        const verifyUser = async() => {
-            const response = await axios.get('/api/admin/verify', {withCredentials: true});
-            if(!response.data.success){
-                navigate('/login');
-            }
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const response = await axios.get("/api/admin/verify", {
+          withCredentials: true,
+        });
+        console.log(response, "response in protected route");
+        if (!response.data.success) {
+          console.log("not verified user");
+          navigate("/login");
         }
-        verifyUser();
-    }, [])
-  return (
-    <div>
-        {children}
-    </div>
-  )
-}
+      } catch (error) {
+        toast.error("Session expired. Please login again.");
+        console.error("Error verifying user:", error);
+        navigate("/login");
+      }
+    };
+    verifyUser();
+  }, []);
+  return <div>{children}</div>;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
