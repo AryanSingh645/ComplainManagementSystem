207526afd3de99db5eb0f23fd51353ac07a94efc
AryanSingh645 <aryansingh645.developer@gmail.com>
2025-04-29 20:06:03 +0530

    fixed bugs

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -4,10 +4,12 @@ import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
+  const [isLoading, setIsLoading] = React.useState(true);
   const navigate = useNavigate();
   useEffect(() => {
     const verifyUser = async () => {
       try {
+        setIsLoading(true);
         const response = await axios.get("https://complainmanagementsystem.onrender.com/api/admin/verify", {
           withCredentials: true,
         });
@@ -20,11 +22,13 @@ const ProtectedRoute = ({ children }) => {
         toast.error("Session expired. Please login again.");
         console.error("Error verifying user:", error);
         navigate("/login");
+      } finally {
+        setIsLoading(false);
       }
     };
     verifyUser();
   }, []);
-  return <div>{children}</div>;
+  return <div>{isLoading ? <p>Loading...</p> : children}</div>;
 };
 
 export default ProtectedRoute;
